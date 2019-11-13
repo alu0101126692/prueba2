cc548a8bd38638de9fb5c3c2e2ace6c6d1ce8cfd
Pablo Torres <alu0101126692@ull.edu.es>
2019-11-13 17:57:46 +0100

    Metodo para obtener el alimento formateado

--- lib/foodie/foodie.rb
+++ lib/foodie/foodie.rb
@@ -1,7 +1,10 @@
 class Alimentos
-	attr_reader :nombre, :gases, :terreno
-	attr_writer :gases, :terreno
+	attr_reader :nombre, :gases, :terreno, :prot, :carbo, :lip
+	attr_writer :gases, :terreno, :prot, :carbo, :lip
 	def initialize (nombre)
 		@nombre = nombre
+	end
+	def to_s
+		"(#{nombre},#{prot},#{carbo},#{lip},#{gases},#{terreno})"
 	end
 end
