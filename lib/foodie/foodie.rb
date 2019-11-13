3267dbba64b86f9e6848bf1935091e4e4154d0ee
Pablo Torres <alu0101126692@ull.edu.es>
2019-11-13 18:53:05 +0100

    Metodo para obtener el impacto ambiental de un hombre

--- lib/foodie/foodie.rb
+++ lib/foodie/foodie.rb
@@ -14,6 +14,14 @@ class Alimentos
 	kcall = lip * 9
 	(kcalc + kcalp + kcall).round(1)
 end
+	def hombre(prot1,prot2,prot3,terr1,terr2,terr3,gas1,gas2,gas3)
+		if (prot1 + prot2 + prot3) >= 54
+	    prot_max = prot1 + prot2 + prot3
+			gases = terr1 + terr2 + terr3
+			terreno = gas1+ gas2+ gas3
 
+		end
+		"(#{prot_max},#{gases},#{terreno})"
+	end
 
 end
