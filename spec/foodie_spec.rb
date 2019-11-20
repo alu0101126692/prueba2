3e8c4fd833c34b9243dbe038866021f91cadb53f
Pablo Torres <alu0101126692@ull.edu.es>
2019-11-20 12:03:57 +0000

    Guard configurado

--- spec/foodie_spec.rb
+++ spec/foodie_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Foodie do
   before(:all) do
     @vaquita = Alimentos.new("Carne de vaca")
   end
-  it "Existe un nombre para el alimento" do
+  it "Existe un nombre para el alimento " do
     @vaquita = Alimentos.new("Carne de vaca")
   end
   it "Existe un metodo para obtener el nombre del Alimento" do
